b940125444f549aa39c19f01abed6d10f2f42917
YuyanTong <99391589+YuyanTong@users.noreply.github.com>
2023-03-03 10:32:23 +0800

    Delete Detect_fluctuation_signal directory

--- Detect_fluctuation_signal/weights_align1.m
+++
@@ -1,9 +0,0 @@
-function [all_weights, reverse_all_weights]=weights_align1(weight_a,weight_b,weight_c,input_dimension)
-post_nodes_num=[weight_a,weight_b,weight_c];
-layer_nodes_num=[input_dimension, post_nodes_num];
-all_weights={};
-reverse_all_weights={};
-for l=2:length(layer_nodes_num)
-    all_weights{l-1}=randn(layer_nodes_num(l-1),layer_nodes_num(l));
-    reverse_all_weights{length(layer_nodes_num)-l+1}=pinv(all_weights{l-1});
-end
